b48bb95fb96df03f3533ff7710c09d0b191c76a2
sbtal <sbtal@gama>
2021-04-28 14:33:23 +0530

    spring apps

---
+++ spring-app-xml/src/main/java/com/demo/spring/Message.java
@@ -0,0 +1,25 @@
+package com.demo.spring;
+
+public class Message {
+
+	private String messageText;
+
+	public Message() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Message(String message) {
+		this.messageText = message;
+	}
+
+	public String getMessageText() {
+		return messageText;
+	}
+
+	public void setMessageText(String messageText) {
+		this.messageText = messageText;
+	}
+
+	
+
+}
