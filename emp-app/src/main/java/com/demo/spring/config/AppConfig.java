486fa1db653ab43663e4c914902bf16156a8ddbc
sbtal <sbtal@gama>
2021-04-29 10:03:33 +0530

    log4j2

--- emp-app/src/main/java/com/demo/spring/config/AppConfig.java
+++ emp-app/src/main/java/com/demo/spring/config/AppConfig.java
@@ -1,10 +1,19 @@
 package com.demo.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.demo.spring.dao.EmpDao;
+import com.demo.spring.dao.EmpDaoImpl;
 
 @Configuration
 @ComponentScan(basePackages = "com.demo.spring")
 public class AppConfig {
+	
+	@Bean
+	public EmpDao empDao3() {
+		return new EmpDaoImpl(); //EmpDao dao=new EmpDaoImpl();
+	}
 
 }
